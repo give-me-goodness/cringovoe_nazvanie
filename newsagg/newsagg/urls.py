dbcc4fb89264a89045be8d302f5a235cf591817a
Alexander Boyko <sasha.boyko.dg.20@gmail.com>
2025-10-07 23:56:09 +0300

    добавил отображение медийки

--- newsagg/newsagg/urls.py
+++ newsagg/newsagg/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -8,3 +10,6 @@ urlpatterns = [
     path('', include('aggregator.urls')),
     #path('', RedirectView.as_view(url='/news/', permanent=False)), #не совсем пон для чего
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
